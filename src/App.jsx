bdc7f46cf11b850959dac397064d619644f6a87f
= <76484677+MuhammadZohaib28@users.noreply.github.com>
2025-09-01 22:01:50 +0500

    first september

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import Header from './components/Header'
+import Header from './components/header/Header'
+import HeroSection from './components/heroSection/HeroSection'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <HeroSection />
     </div>
   )
 }
